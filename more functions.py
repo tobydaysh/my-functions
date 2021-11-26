fc732e583c65904541ee661832e207f221eda7d4
Toby <94986276+tobydaysh@users.noreply.github.com>
2021-11-26 10:25:08 +0000

    Add files via upload

---
+++ more functions.py
@@ -0,0 +1,13 @@
+def rannum():
+    numb = str(input('input name'))
+    if numb == 'toby':
+        print('welcome commander bond')
+    else:
+        print('try again')
+    return
+
+
+rannum()
+
+
+
