684eb768af3129ffb09442d85f3d50718701753f
Toby <94986276+tobydaysh@users.noreply.github.com>
2022-02-08 16:56:03 +0000

    Add files via upload
    more functions that i made a while ago

---
+++ more functions.py
@@ -0,0 +1,25 @@
+def function_1():  # this defines the function name
+    name = input('input name')
+    if name == ' toby':
+        print('welcome Toby')
+    else:
+        print('try again')
+    return
+    
+
+function_1()  # this calls the function name, which runs the code above
+
+
+
+
+
+def function_2():
+    name = input('input name')
+    if name == ' alan':
+        print('welcome dad')
+    else:
+        print('try again dad')
+    return
+
+function_2()
+
